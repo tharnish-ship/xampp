7d28da4f109f2ad290274cbf95a54650a48be51b
tobyharnish <tharnish214@gmail.com>
2020-11-20 18:56:23 -0500

    project

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php include('server.php') ?>
+<?php include('app_logic.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+		<?php include('messages.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+<p>
+<a href="enter_email.php">
+For Got Your Password?
+</a>
+</p>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
